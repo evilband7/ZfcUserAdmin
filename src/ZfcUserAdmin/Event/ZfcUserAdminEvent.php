8a86228d9d8922e9eabd02687360b2bd8dd7019a
MOXGA <map.siwapun@gmail.com>
2015-06-25 07:30:46 +0700

    modify ZfcUserAdmin\Event\ZfcUserAdminEvent class

--- src/ZfcUserAdmin/Event/ZfcUserAdminEvent.php
+++ src/ZfcUserAdmin/Event/ZfcUserAdminEvent.php
@@ -1,10 +1,30 @@
 <?php
 namespace ZfcUserAdmin\Event;
 
-class ZfcUserAdminEvent
+use Zend\EventManager\Event;
+use Zend\View\Renderer\PhpRenderer;
+use ZfcUser\Entity\UserInterface;
+
+class ZfcUserAdminEvent extends  Event
 {
     static $IDENTIFIERS = array('ZfcUserAdmin');
-    static $EVENT_RENDER = 'render';
+    static $EVENT_RENDER_BUTTON = 'ZfcUserAdmin.renderButton';
+    
+    /**
+     * 
+     * @return UserInterface
+     */
+    public function getUser(){
+        return $this->getParam('user');
+    }
+    
+    /**
+     * 
+     * @return PhpRenderer
+     */
+    public function getPhpRenderer(){
+        return $this->getParam('phpRenderer');
+    }
 }
 
 ?>
